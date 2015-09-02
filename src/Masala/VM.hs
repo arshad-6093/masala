6b2583d33f8a7709867a2006dd2d6314ef4a6f6e
Stuart Popejoy <spopejoy@panix.com>
2015-09-01 20:31:48 -0400

    Cleanup for raftBFT. Better xApi ux.

--- src/Masala/VM.hs
+++ src/Masala/VM.hs
@@ -1,9 +1,11 @@
+{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DeriveFunctor #-}
 
 module Masala.VM where
 
@@ -11,9 +13,9 @@ import Data.Bits
 import Data.Word
 import Control.Monad.Reader
 import Control.Monad.State.Strict
-import Control.Lens
+import Control.Lens hiding (op)
 import Masala.Instruction
-import Control.Monad.Error
+import Control.Monad.Except
 import qualified Data.Vector as V
 import Control.Applicative
 import Prelude hiding (LT,GT,EQ,log)
@@ -29,7 +31,7 @@ import qualified Data.Set as S
 data Prog = Prog {
       -- parsed bytecode
       pCode :: V.Vector ByteCode
-      -- map of valid codepoints to pCode indexesx
+      -- map of valid codepoints to pCode indexes
     , pCodeMap :: M.Map U256 Int
 }
 
@@ -71,19 +73,66 @@ acctBalance f s = fmap (\a -> s { _acctBalance = a }) (f $ _acctBalance s)
 acctAddress :: Lens' ExtAccount U256
 acctAddress f s = fmap (\a -> s { _acctAddress = a }) (f $ _acctAddress s)
 
+newtype ExtOp e a = ExtOp { runExtOp :: e -> (a, e) } deriving (Functor)
+instance Applicative (ExtOp e) where
+    pure a = ExtOp (a,)
+    (ExtOp f) <*> (ExtOp a) = ExtOp $ \e -> ((fst $ f e) (fst $ a e),e)
+instance Monad (ExtOp e) where
+    return = pure
+    (ExtOp a) >>= f = ExtOp $ \e -> runExtOp (f (fst $ a e)) e
+
+
+execExtOp :: ExtOp e a -> e -> e
+execExtOp op = snd . runExtOp op
+
+evalExtOp :: ExtOp e a -> e -> a
+evalExtOp op = fst . runExtOp op
+
+setExt :: (e -> e) -> ExtOp e ()
+setExt f = ExtOp $ ((),) . f
+
+useExt :: (e -> a) -> ExtOp e a
+useExt f = fmap f getExt
+
+getExt :: ExtOp e e
+getExt = ExtOp $ \e -> (e,e)
+
+xover :: Profunctor p => Setting p s s a b -> p a b -> ExtOp s ()
+xover l f = setExt $ over l f
+
+xfirstOf :: Getting (Leftmost a) s a -> ExtOp s (Maybe a)
+xfirstOf l = useExt $ firstOf l
+
+xview :: Getting b s b -> ExtOp s b
+xview l = view l <$> getExt
+
+xApply :: VM m e => (Ext e -> f) -> (f -> ExtOp e a) -> m a
+xApply acc g = do
+  f <- acc <$> reader extApi
+  (a,e) <- runExtOp (g f) <$> use ext
+  ext .= e
+  return a
+
+-- | work in progress to use applicative??
+appEx :: VM m e => (Env e -> a) -> m (ExtOp e a)
+appEx acc = pure <$> reader acc
+
+
+
+
+
+
 
 data Ext e = Ext {
-      xStore :: U256 -> U256 -> U256 -> e -> e
-    , xLoad :: U256 -> U256 -> e -> Maybe U256
-    , xAddress :: U256 -> e -> Maybe ExtAccount
-    , xCreate :: Gas -> e -> (ExtAccount,e)
-    , xSaveCode :: U256 -> [Word8] -> e -> e
-    , xSuicide :: U256 -> e -> (Bool, e)
-    , xRefund :: Gas -> e -> e
-    , xIsCreate :: U256 -> e -> Bool
+      xStore :: U256 -> U256 -> U256 -> ExtOp e ()
+    , xLoad :: U256 -> U256 -> ExtOp e (Maybe U256)
+    , xAddress :: U256 -> ExtOp e (Maybe ExtAccount)
+    , xCreate :: Gas -> ExtOp e ExtAccount
+    , xSaveCode :: U256 -> [Word8] -> ExtOp e ()
+    , xSuicide :: U256 -> ExtOp e Bool
+    , xRefund :: Gas -> ExtOp e ()
+    , xIsCreate :: U256 -> ExtOp e Bool
 }
-
-
 
 data Resume e = Resume {
       rPush :: U256,
@@ -95,6 +144,7 @@ data Resume e = Resume {
 
 data Env e = Env {
       debug :: Bool
+    , doGas :: Bool
     , callData :: V.Vector U256
     , extApi :: Ext e
     , prog :: Prog
@@ -142,6 +192,7 @@ data ControlFlow e =
         | Return [Word8]
         | Yield VMResult
     deriving (Show)
+
 
 
 
@@ -193,7 +244,7 @@ emptyState = VMState [] 0 M.empty
 
 runVM :: (MonadIO m, Functor m, Show ext) =>
          VMState ext -> Env ext -> Maybe (Resume ext) -> m (Either String (Output ext))
-runVM vm env callR = evalStateT (runReaderT (runErrorT go) env) vm >>= postEx env
+runVM vm env callR = evalStateT (runReaderT (runExceptT go) env) vm >>= postEx env
     where go = (,) <$> stepVM callR <*> get
 
 
@@ -229,9 +280,7 @@ stepVM r = do
               ext .= e
               case action of
                 SaveMem loc len -> mstores loc 0 len (V.fromList $ w8sToU256s result)
-                SaveCode addr -> do
-                            f <- xSaveCode <$> reader extApi
-                            ext %= f addr result
+                SaveCode addr -> xApply xSaveCode (\f -> f addr result)
               push p
               return Next
   case cf of
@@ -280,18 +329,20 @@ handleGas i ps svs = do
 
 deductGas :: VM m e => Gas -> m ()
 deductGas total = do
-  pg <- use gas
-  let gas' = pg - total
-  if gas' < 0
-  then do
-    gas .= 0
-    throwError $ "Out of gas, gas=" ++ show pg ++
-                   ", required=" ++ show total ++
-                   ", balance= " ++ show gas'
-  else do
-    d <- reader debug
-    when d $ liftIO $ putStrLn $ "gas used: " ++ show total
-    gas .= gas'
+  enabled <- reader doGas
+  when enabled $ do
+    pg <- use gas
+    let gas' = pg - total
+    if gas' < 0
+    then do
+      gas .= 0
+      throwError $ "Out of gas, gas=" ++ show pg ++
+                 ", required=" ++ show total ++
+                 ", balance= " ++ show gas'
+    else do
+      d <- reader debug
+      when d $ liftIO $ putStrLn $ "gas used: " ++ show total
+      gas .= gas'
 
 computeMemGas :: VM m e => U256 -> m Gas
 computeMemGas newSzBytes = do
@@ -305,9 +356,7 @@ computeMemGas newSzBytes = do
 
 
 refund :: VM m e => Gas -> m ()
-refund g = do
-  f <- xRefund <$> reader extApi
-  ext %= f g
+refund g = xApply xRefund ($ g)
 
 computeStoreGas :: VM m e => U256 -> U256 -> m Gas
 computeStoreGas l v' = do
@@ -322,8 +371,7 @@ computeStoreGas l v' = do
 computeCallGas :: VM m e => Maybe U256 -> m Gas
 computeCallGas Nothing = return 0
 computeCallGas (Just a) = do
-  f <- xIsCreate <$> reader extApi
-  isNew <- f a <$> use ext
+  isNew <- xApply xIsCreate ($ a)
   return $ if isNew then gas_callnewaccount else 0
 
 
@@ -418,22 +466,18 @@ mstores' memloc off len v =
 sload :: VM m e => U256 -> m U256
 sload i = do
   s <- reader address
-  f <- xLoad <$> reader extApi
-  fromMaybe 0 . f s i <$> use ext
+  fromMaybe 0 <$> xApply xLoad (\f -> f s i)
 
 sstore :: VM m e => U256 -> U256 -> m ()
 sstore a b = do
   s <- reader address
-  f <- xStore <$> reader extApi
-  ext %= f s a b
+  xApply xStore (\f -> f s a b)
 
 toAddy :: U256 -> U256
 toAddy = (`mod` (2 ^ (160 :: Int)))
 
 addy :: VM m e => U256 -> m (Maybe ExtAccount)
-addy k = do
-  f <- xAddress <$> reader extApi
-  f (toAddy k) <$> use ext
+addy k = xApply xAddress ($ toAddy k)
 
 
 int :: Integral a => a -> Int
@@ -452,8 +496,7 @@ codeCopy memloc codeoff len codes = mstores memloc 0 (V.length us) us
 
 lookupAcct :: VM m e => String -> U256 -> m ExtAccount
 lookupAcct msg addr = do
-  lf <- xAddress <$> reader extApi
-  l <- lf addr <$> use ext
+  l <- xApply xAddress ($ addr)
   maybe (throwError $ msg ++ ": " ++ show addr) return l
 
 doCall :: VM m e => Gas -> U256 -> U256 -> Gas ->
@@ -469,9 +512,7 @@ doCall cgas addr codeAddr cgaslimit inoff inlen outoff outlen = do
 create :: VM m e => Gas -> U256 -> U256 -> m (ControlFlow e)
 create cgas codeloc codeoff = do
   codes <- concatMap u256ToW8s <$> mloads codeloc codeoff
-  createf <- xCreate <$> reader extApi
-  (newaddy,ext') <- createf cgas <$> use ext
-  ext .= ext'
+  newaddy <- xApply xCreate ($ cgas)
   deductGas cgas
   gl <- reader gaslimit
   return  $ Yield Call { cGas = cgas, cAcct = newaddy, cCode = codes, cGasLimit = gl,
@@ -479,9 +520,7 @@ create cgas codeloc codeoff = do
 
 suicide :: VM m e => U256 -> m (ControlFlow e)
 suicide addr = do
-  f <- xSuicide <$> reader extApi
-  (isNewSuicide,m') <- f addr <$> use ext
-  ext .= m'
+  isNewSuicide <- xApply xSuicide ($ addr)
   when isNewSuicide $ refund gas_suicide
   return Stop
 
@@ -568,7 +607,7 @@ run_ = runBC_ . parseHex
 
 runBC_ :: ToByteCode a => [a] -> IO (Either String (Output TestExtData))
 runBC_ bc = runVM (emptyState ex gas')
-            (Env dbug calldata testExt
+            (Env dbug enableGas calldata testExt
              (toProg tbc)
              (_acctAddress acc)
              addr
@@ -577,6 +616,7 @@ runBC_ bc = runVM (emptyState ex gas')
             Nothing
     where tbc = concatMap toByteCode bc
           addr = 123456
+          enableGas = True
           gas' = 10000000
           acc = ExtAccount (bcsToWord8s tbc) 0 addr M.empty
           ex = TestExtData (M.fromList [(addr,acc)]) S.empty S.empty 0
@@ -584,23 +624,25 @@ runBC_ bc = runVM (emptyState ex gas')
           dbug = True
           testExt :: Ext TestExtData
           testExt = Ext {
-                      xStore = \a k v -> over (edAccts . ix a . acctStore) (M.insert k v)
-                    , xLoad = \a k -> firstOf (edAccts . ix a . acctStore . ix k)
-                    , xAddress = \k -> firstOf (edAccts . ix k)
-                    , xCreate = \g e ->
-                                let newaddy = succ (maximum (M.keys (view edAccts e)))
-                                    newacct = ExtAccount [] g newaddy M.empty
-                                    e' = over edCreates (S.insert newaddy) .
-                                         over edAccts (M.insert newaddy newacct) $ e
-                                in (newacct,e')
-                    , xSaveCode = \a ws -> set (edAccts . ix a . acctCode) ws
-                    , xSuicide = \a e ->
-                                  let justDeleted = a `S.member` view edSuicides e
-                                      e' = over edSuicides (S.delete a) e
-                                  in (justDeleted,e')
-                    , xRefund = \a -> over edRefund (+ a)
-                    , xIsCreate = \a -> S.member a . view edCreates
+                      xStore = \a k v -> xover (edAccts . ix a . acctStore) (M.insert k v)
+                    , xLoad = \a k -> xfirstOf (edAccts . ix a . acctStore . ix k)
+                    , xAddress = \k -> xfirstOf (edAccts . ix k)
+                    , xCreate = \g -> do
+                                  newaddy <- succ . maximum . M.keys <$> xview edAccts
+                                  let newacct = ExtAccount [] g newaddy M.empty
+                                  xover edCreates (S.insert newaddy)
+                                  xover edAccts (M.insert newaddy newacct)
+                                  return newacct
+                    , xSaveCode = \a ws -> setExt $ set (edAccts . ix a . acctCode) ws
+                    , xSuicide = \a -> do
+                                  justDeleted <- S.member a <$> xview edSuicides
+                                  xover edSuicides (S.delete a)
+                                  return justDeleted
+                    , xRefund = \a -> xover edRefund (+ a)
+                    , xIsCreate = \a -> S.member a <$> xview edCreates
                     }
+
+
 
 data TestExtData = TestExtData {
       _edAccts :: M.Map U256 ExtAccount
